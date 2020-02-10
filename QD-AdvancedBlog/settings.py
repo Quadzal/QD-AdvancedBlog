237498dc0bf568113b863ccab67d123b91cda641
Quadzal <40356538+Quadzal@users.noreply.github.com>
2020-02-10 22:04:03 +0300

    Update settings.py

--- QD-AdvancedBlog/settings.py
+++ QD-AdvancedBlog/settings.py
@@ -162,7 +162,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'esat3515@gmail.com'
-EMAIL_HOST_PASSWORD = '123ab123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
